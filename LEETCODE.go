d52c597ba4e219b83774ae49d059acfc8ecd3bbc
SafarovOrkhan <safarov.on@gmail.com>
2025-04-17 01:17:33 +0400

    update

--- LEETCODE.go
+++ LEETCODE.go
@@ -1,30 +1,36 @@
 package main
 
 // https://leetcode.com/problems/multiply-strings/description/
-import "fmt"
+import (
+	"fmt"
+)
 
-func multiply(num1 string, num2 string) string {
-	sheet := make(map[byte]int)
-	sheet[48] = 0
-	sheet[49] = 1
-	sheet[50] = 2
-	sheet[51] = 3
-	sheet[52] = 4
-	sheet[53] = 5
-	sheet[54] = 6
-	sheet[55] = 7
-	sheet[56] = 8
-	sheet[57] = 9
+func plusOne(digits []int) []int {
+	increaser := 1
+	total := 0
+	for i := len(digits) - 1; i >= 0; i-- {
+		fmt.Println(digits[i])
+		total += digits[i] * increaser
+		increaser *= 10
+	}
+	var slc []int
+	num := total + 1
 
-	num1_slice := []byte(num1)
-	num2_slice := []byte(num2)
+	for num > 0 {
+		slc = append([]int{num % 10}, slc...)
+		num /= 10
+	}
 
-	// for i , val := range sheet {
-
-	// }
+	return slc
 }
 
 func main() {
-	var value []byte = []byte{1, 2, 3}
-	fmt.Println([]int(value))
+	var digits []int
+	digits = []int{7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6}
+	// digits = []int{4, 3, 2, 1}
+	// digits = []int{9}
+	// fmt.Println("ada", 1/1)
+	res := plusOne(digits)
+
+	fmt.Println(res)
 }
